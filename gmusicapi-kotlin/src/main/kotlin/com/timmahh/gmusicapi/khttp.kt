9467db32fdf45722310248a0dcf0370ba1f9b933
Timothy Logan <timtimmahh@gmail.com>
2018-04-21 06:39:48 -0400

    Trying a new commit.

---
+++ gmusicapi-kotlin/src/main/kotlin/com/timmahh/gmusicapi/khttp.kt
@@ -0,0 +1,5 @@
+package com.timmahh.gmusicapi
+
+/**
+ * Entity and Pojo used to create a many to many relationship since Room doesn't support it.
+ */
---
+++ gmusicapi-kotlin/src/main/kotlin/com/timmahh/gmusicapi/protocol/NetServiceCaller.kt
@@ -0,0 +1,5 @@
+package com.timmahh.gmusicapi.protocol
+
+/**
+ * Entity and Pojo used to create a many to many relationship since Room doesn't support it.
+ */
